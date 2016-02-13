357c7ffa12e0010d4ce492ee38bea7dea03a76a4
kkcmah <kelbymah@hotmail.com>
2016-02-13 14:58:31 -0700

    reads programs from a file

--- test.py
+++ test.py
@@ -1,14 +1,44 @@
-import sys, subprocess
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+import time
+#process names from text file stored into array
+#sleep after each check for 2 secs
+
 
 def main():
-    p = subprocess.Popen([sys.argv[1]])
-    p2 = subprocess.Popen([sys.argv[2], sys.argv[3]])
-
+    #read program names from file and put them into an array
+    fileName = open(sys.argv[1])
+    programs = fileName.read().splitlines()
+    fileName.close()
+    
+    #start up programs in separate processes
+    p0 = subprocess.Popen(programs[0])
+    p1 = subprocess.Popen(programs[1])
+    '''
+    p2 = subprocess.Popen(programs[2])
+    p3 = subprocess.Popen(programs[3])
+    '''
+            
+    #check processes and respond accordingly
     while True:
-        if (p.poll() is not None):
-            print sys.argv[1] + " died"
-            p = subprocess.Popen([sys.argv[1]])
-            p2 = subprocess.Popen([sys.argv[2], sys.argv[3]])
-
+        if (p0.poll() is not None):
+            #print(programs[0] + " died")
+            p0 = subprocess.Popen(programs[0])
+        if (p1.poll() is not None):
+            #print(programs[1] + " died")
+            p1 = subprocess.Popen(programs[1]) 
+        '''
+        if (p2.poll() is not None):
+            #print(programs[2] + " died")
+            p2 = subprocess.Popen(programs[2])
+        if (p3.poll() is not None):
+            #print(programs[3] + " died")
+            p3 = subprocess.Popen(programs[3])
+        '''
+        
+        time.sleep(2)
+        
 if __name__ == '__main__':
     main()
