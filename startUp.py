bad6ae51bcbf920af1d245f38bd73e630de72cbc
kkcmah <kelbymah@hotmail.com>
2016-02-19 13:09:55 -0700

    changed to work with any number of processes

--- startUp.py
+++ startUp.py
@@ -13,51 +13,25 @@ def main():
     programs = fileName.read().splitlines()
     fileName.close()
     
-    #start up programs in separate processes
-    p0 = subprocess.Popen(programs[0])
-    p1 = subprocess.Popen(programs[1])
-    '''
-    p2 = subprocess.Popen(programs[2])
-    p3 = subprocess.Popen(programs[3])
-    '''
+    numberOfPrograms = programs.__len__()
+    MAX_RESTART = 10
+    timesRestarted = [0] * numberOfPrograms
     
-    MAXRESTART = 10
-    timesRestarted = [0, 0, 0, 0]
+    #start up programs in separate processes
+    processes = []
+    for i in range(numberOfPrograms):
+        processes.append(subprocess.Popen(programs[i]))
     
     #check processes and respond accordingly
     while True:
-        if(p0.poll() is not None):
-            #print(programs[0], "died")
-            p0 = subprocess.Popen(programs[0])
-            timesRestarted[0] += 1
-            if (timesRestarted[0] > MAXRESTART):
-                print(programs[0], "exceeded", MAXRESTART, "restarts")
-                return
-        if(p1.poll() is not None):
-            #print(programs[1], "died")
-            p1 = subprocess.Popen(programs[1])
-            timesRestarted[1] += 1
-            if (timesRestarted[1] > MAXRESTART):
-                print(programs[1], " exceeded", MAXRESTART, " restarts")
-                return     
-        '''
-        if(p2.poll() is not None):
-            #print(programs[2], "died")
-            p2 = subprocess.Popen(programs[2])
-            timesRestarted[2] += 1
-            if (timesRestarted[2] > MAXRESTART):
-                print(programs[2], "exceeded", MAXRESTART, "restarts")
-                return
-        if(p3.poll() is not None):
-            #print(programs[3], "died")
-            p3 = subprocess.Popen(programs[3])
-            timesRestarted[3] += 1
-            if (timesRestarted[3] > MAXRESTART):
-                print(programs[3], "exceeded", MAXRESTART, "restarts")
-                return  
-        '''
-        
+        for i in range(numberOfPrograms):
+            if(processes[i].poll() is not None):
+                if(timesRestarted[i] > MAX_RESTART):
+                    print(programs[i], "exceeded", MAX_RESTART, "restarts")
+                    return
+                processes[i] = subprocess.Popen(programs[i])
+                timesRestarted[i] += 1          
         time.sleep(2)
-        
+    
 if __name__ == '__main__':
     main()
