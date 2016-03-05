2a3249f4132bc4b3ce7252b0787eeeae172dfd1d
kkcmah <kelbymah@hotmail.com>
2016-03-05 11:23:26 -0700

    Added basic logging and moved main to class

--- solar_car_process.py
+++ solar_car_process.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
 import subprocess
+import logging
+import shlex
+import time
+
+import settings
+
 
 class SolarCarProcess:
         
@@ -15,4 +22,26 @@ class SolarCarProcess:
     
     def check_status(self):
         return self.process.poll()
+
+    
+class Domovoi:
         
+    def run(self):
+        logging.basicConfig(filename = sys.argv[2], format = '%(asctime)s - %(levelname)s - %(message)s')
+        with open(sys.argv[1]) as file:
+            solar_car_processes = [SolarCarProcess(shlex.split(path)) for path in file.read().splitlines()]
+        
+        # Check processes and respond accordingly
+        while True:
+            for solar_car_process in solar_car_processes:
+                if solar_car_process.check_status():
+                    if solar_car_process.timesRestarted == settings.MAX_RESTART:
+                        logging.critical("%s reached %d restart(s) with signal %d",
+                            solar_car_process.path, settings.MAX_RESTART, solar_car_process.process.returncode)
+                        solar_car_processes.remove(solar_car_process)
+                    else:
+                        logging.warning("%s crash number %d with signal %d",
+                            solar_car_process.path, solar_car_process.timesRestarted + 1, solar_car_process.process.returncode)
+                        solar_car_process.start()
+            time.sleep(settings.SLEEP_TIME)
+    
--- start_up.py
+++ start_up.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python3
 
-import sys
-import time
+#import sys
 
-import settings
-from solar_car_process import SolarCarProcess
+from solar_car_process import Domovoi
 
 def main():
-    with open(sys.argv[1]) as file:
-        solar_car_processes = [SolarCarProcess(path) for path in file.read().splitlines()]
-
-    # Check processes and respond accordingly
-    while True:
-        for solar_car_process in solar_car_processes:
-            if solar_car_process.check_status():
-                if solar_car_process.timesRestarted == settings.MAX_RESTART:
-                    print(solar_car_process.path, "reached", settings.MAX_RESTART, "restarts")
-                    solar_car_processes.remove(solar_car_process)
-                else:
-                    solar_car_process.start()
-        time.sleep(settings.SLEEP_TIME)
+    domovoi = Domovoi()
+    domovoi.run()
         
 if __name__ == '__main__':
     main()
