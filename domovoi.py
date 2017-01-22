c4b5332c2319a46e4de0e11746f7ece86bbf24f0
Baba <baba.adeniran@gmail.com>
2017-01-21 18:05:58 -0700

    fixed problem of domovoi not exiting with no processes in list

--- domovoi.py
+++ domovoi.py
@@ -58,7 +58,7 @@ class Domovoi:
         self.check_paths(solar_car_processes)
         self.start_processes(solar_car_processes)
         # Watch over the processes and respond accordingly
-        while True:
+        while len(solar_car_processes):
             for solar_car_process in solar_car_processes:
                 if solar_car_process.check_status():
                     if solar_car_process.process.returncode == 0: # Good exit, removes the process from the list
