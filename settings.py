6831b7ef5e182a53893a809c76415b25afa539d6
bill-luu <billluu619@gmail.com>
2017-11-03 22:22:16 -0600

    Put placeholder ip

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ MAX_RESTART = 2
 SLEEP_TIME = 2
 SECONDARY_FILE = "secondary.txt"
 PRIMARY_FILE = "primary.txt"
-HOST_IP = "google.com"
+HOST_IP = "239.255.43.21" # Placeholder, taken from Hermes/config.ini.example 
 
